51361e5b1de0c709103b918e9d47ca1c94c2edbc
joshjun010202@gmail.com <joshjun010202@gmail.com>
2020-09-05 23:29:16 +0900

    Added basic sideways movement

--- MeepAdventureGame /Assets/Scripts/PlayerController.cs
+++ MeepAdventureGame /Assets/Scripts/PlayerController.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Vector3 change;
+    private RigidBody2D myRigidbody;
+    private Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+		myRigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void FixedUpdate() {
+
+    }
+
+    void HandleMovement() {
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+
+        if (change != Vector3.zero) {
+            change.Normalize();
+            myRigidbody.MovePosition(transform.position + change * moveSpeed * Time.deltaTime);
+        }
     }
 }
