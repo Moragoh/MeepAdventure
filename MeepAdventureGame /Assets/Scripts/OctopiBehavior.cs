26aa8ffcd5b66e8cbe3e2711a44d2b6419db3332
joshjun010202@gmail.com <joshjun010202@gmail.com>
2020-09-08 23:13:57 +0900

    Death added, enemy added, working on UI box

---
+++ MeepAdventureGame /Assets/Scripts/OctopiBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OctopiBehavior : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+
+    private int direction = 1;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveOctopus();
+    }
+
+    void MoveOctopus()
+    {
+        float moveBy = direction * moveSpeed;
+
+        rb.velocity = new Vector2(moveBy, 0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "return")
+        {
+            direction *= -1;
+        }
+    }
+}
--- MeepAdventureGame /Assets/Scripts/PlayerController.cs
+++ MeepAdventureGame /Assets/Scripts/PlayerController.cs
@@ -9,16 +9,21 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private bool onGround;
     [SerializeField] Transform onGroundChecker;
     [SerializeField] float groundCheckRadius;
-    [SerializeField] public LayerMask groundLayer;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private GameObject spawnPoint;
+    [SerializeField] private GameObject deathAnim;
 
     private Vector3 change;
     private Rigidbody2D myRigidbody;
     private Animator anim;
+    private bool movingRight = true;
+    private bool canMove = true;
+    private bool isDying = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        //anim = GetComponent<Animator>();
+        anim = GetComponent<Animator>();
 		myRigidbody = GetComponent<Rigidbody2D>();
         onGround = true;
     }
@@ -34,6 +39,44 @@ public class PlayerController : MonoBehaviour
 
     }
 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.tag == "death")
+        {
+            canMove = false;
+
+            if (isDying == false)
+            {
+                StartCoroutine(PlayDeathAndRespawnCo());
+            }
+          
+        }
+    }
+
+    // Coroutine for death animation and respawn
+    private IEnumerator PlayDeathAndRespawnCo()
+    {
+        isDying = true;
+
+        // Turns player invisible
+        gameObject.GetComponent<SpriteRenderer>().enabled = false;
+
+        // Instantiate
+        GameObject effect = Instantiate(deathAnim, transform.position, Quaternion.identity);
+        Destroy(effect, 1f);
+
+        // Wait seconds
+        yield return new WaitForSeconds(1.2f);
+
+        // Teleport to spawn
+        this.transform.position = spawnPoint.transform.position;
+        gameObject.GetComponent<SpriteRenderer>().enabled = true;
+
+        isDying = false;
+        canMove = true;
+    }
+
     void CheckIfOnGround()
     {
         Collider2D collider = Physics2D.OverlapCircle(onGroundChecker.position, groundCheckRadius, groundLayer);
@@ -41,10 +84,12 @@ public class PlayerController : MonoBehaviour
         if (collider != null)
         {
             onGround = true;
+            anim.SetBool("isJumping", false);
         }
         else if (collider == null)
         {
             onGround = false;
+            anim.SetBool("isJumping", true);
         }
     }
 
@@ -52,16 +97,41 @@ public class PlayerController : MonoBehaviour
         change.x = Input.GetAxisRaw("Horizontal");
         float moveBy = change.x * moveSpeed;
 
-        if (change != Vector3.zero) {
+        if (change != Vector3.zero && canMove) {
             //change.Normalize();
             //myRigidbody.MovePosition(transform.position + change * moveSpeed * Time.deltaTime);
+            anim.SetBool("isMoving", true);
             myRigidbody.velocity = new Vector2(moveBy, myRigidbody.velocity.y);
         }
-
-        if (Input.GetButtonDown("Jump") == true && onGround)
+        else
         {
-            print("jumped");
+            anim.SetBool("isMoving", false);
+        }
+
+        if (Input.GetButtonDown("Jump") == true && onGround && canMove)
+        {
+            // Perform jump
             myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
         }
+
+        // Animate player walking to the right or left
+        if (change.x > 0 && !movingRight)
+        {
+            FlipPlayer();
+        }
+        else if (change.x < 0 && movingRight)
+        {
+            FlipPlayer();
+        }
     }
+
+    void FlipPlayer()
+    {
+        movingRight = !movingRight;
+
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+
 }
